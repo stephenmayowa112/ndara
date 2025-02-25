a4745ccd8e89f72c359d5ac706e30c3f97d6ff44
stephenmayowa112 <stephenmayowa112@gmail.com>
2025-02-25 08:21:13 +0100

    FEAT: adjust padding in HeroSection component and change chat button positioning from fixed to absolute

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -104,7 +104,7 @@ const HeroSection = () => {
       </div>
 
       {/* Main Hero Content */}
-      <div className="flex flex-col md:flex-row h-screen">
+      <div className="flex flex-col md:flex-row h-screen pt-32 md:pt-0">
         {/* Text Content (Left Side) */}
         <div className="w-full md:w-1/2 flex flex-col justify-center px-8 md:px-16 z-10">
           <h1 className="text-[#43503F] text-3xl md:text-4xl lg:text-5xl font-medium leading-tight mb-4">
@@ -185,7 +185,7 @@ const HeroSection = () => {
       </div>
 
       {/* Chat Button */}
-      <div className="fixed right-6 bottom-6 z-20">
+      <div className="absolute right-6 bottom-6 z-20">
         <button className="bg-[#43503F] hover:bg-opacity-90 text-white px-4 py-2 rounded-lg flex items-center">
           <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
             <path fillRule="evenodd" d="M18 10c0 3.866-3.582 7-8 7a8.841 8.841 0 01-4.083-.98L2 17l1.338-3.123C2.493 12.767 2 11.434 2 10c0-3.866 3.582-7 8-7s8 3.134 8 7zM7 9H5v2h2V9zm8 0h-2v2h2V9zM9 9h2v2H9V9z" clipRule="evenodd" />
